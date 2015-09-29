f9f9ee5bfcf5ae024cfca319c75809399428b5dc
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-29 15:36:36 -0700

    Add LinkedList#remove

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -20,4 +20,25 @@ class LinkedList
     end
     current
   end
+
+  def remove(val)
+    current = @head
+    if current.value == val
+      @head = current.next_node
+      return current
+    end
+    while current.value != val
+      if current.next_node.value == val && current.next_node.next_node != nil
+        removed = current.next_node
+        current.next_node = current.next_node.next_node
+        return removed
+      elsif current.next_node.value == val && current.next_node.next_node.nil?
+        removed = current.next_node
+        current.next_node = nil
+        return removed
+      end
+      current = current.next_node
+      return nil if current.next_node.nil?
+    end
+  end
 end
