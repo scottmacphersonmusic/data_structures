f8d667a7975a511c3763bc4994a97f64c6753fe5
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-29 14:02:38 -0700

    Write spec for SinglyLinkedList object

---
+++ spec/singly_linked_list_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'singly_linked_list'
+
+describe SinglyLinkedList do
+  it "has @head" do
+    list = SinglyLinkedList.new "value"
+    list.head.must_be_instance_of Node
+    list.head.value.must_equal "value"
+  end
+end
