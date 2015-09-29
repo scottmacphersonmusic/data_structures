33cdeb29ebc2599cb8fed3806d151fd3b7330f2e
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-29 14:59:16 -0700

    Add LinkedList#search(val)
    Test unhappy path

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -11,4 +11,13 @@ class LinkedList
     node.next_node = @head
     @head = node
   end
+
+  def search(val)
+    current = @head
+    while current.value != val
+      current = current.next_node
+      return nil if current.next_node.nil?
+    end
+    current
+  end
 end
