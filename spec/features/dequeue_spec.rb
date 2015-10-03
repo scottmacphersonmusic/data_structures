6cc413aa96f277c26c4ac1e9262d45726f98517f
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-02 17:26:46 -0700

    Write dequeue spec

---
+++ spec/features/dequeue_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'scoqueue'
+
+describe ScoQueue do
+  it "can dequeue tasks" do
+    q = ScoQueue.new "one"
+    q.enqueue "two"
+    q.enqueue "three"
+    # @head =  [val: three, nxt: [val: two, nxt: [val: one, nxt: nil]]]
+    de_q = q.dequeue
+    # @head = [val: two, nxt: [val: one, nxt: nil]]
+    de_q.val.must_equal "one"
+    q.head.val.must_equal "three"
+    q.head.nxt.val.must_equal "two"
+
+    de_q = q.dequeue
+    # @head = [val: one, nxt: nil]
+    de_q.val.must_equal "two"
+    q.head.val.must_equal "three"
+    q.head.nxt.must_equal nil
+  end
+
+  it "raises exception on empty queue dequeue" do
+    q = ScoQueue.new "one"
+    # @head = [val: one, nxt: nil]
+    q.dequeue
+    # @head = nil
+    proc { q.dequeue }.must_raise NoMethodError
+  end
+end
