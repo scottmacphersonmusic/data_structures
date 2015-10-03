34e7361a773476599d6ac2201d0969931dea67b6
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-03 10:59:00 -0700

    Write insertion_sort spec

---
+++ spec/features/insertion_sort_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'insertion_sort'
+
+describe 'Insertion Sort' do
+  it "sorts an array of numbers" do
+    a = (1..10).to_a.shuffle
+    a.insertion_sort.must_equal (1..10).to_a
+  end
+
+  it "returns array if lenght == 1" do
+    [7].insertion_sort.must_equal [7]
+  end
+end
