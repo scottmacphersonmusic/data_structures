0cb8a80cc88919be53b35f6f0de4411f21f26723
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-02 17:38:14 -0700

    Update enqueue spec to not use @tail

--- spec/features/enqueue_spec.rb
+++ spec/features/enqueue_spec.rb
@@ -4,17 +4,18 @@ require 'scoqueue'
 describe ScoQueue do
   it "can enqueue task" do
     q = ScoQueue.new "First Task"
-
+    # @head = [val: "First Task", nxt: nil]
     q.head.val.must_equal "First Task"
     q.head.nxt.must_equal nil
-    q.tail.val.must_equal "First Task"
 
     q.enqueue "Second Task"
     q.enqueue "Third Task"
-
+    # @head = [val: "Third Task", nxt:
+    #          [val: "Second Task", nxt:
+    #           [val: "First Task", nxt: nil]]]
     q.head.val.must_equal "Third Task"
     q.head.nxt.val.must_equal "Second Task"
-    q.tail.val.must_equal "First Task"
-    q.tail.nxt.must_equal nil
+    q.head.nxt.nxt.val.must_equal "First Task"
+    q.head.nxt.nxt.nxt.must_equal nil
   end
 end
