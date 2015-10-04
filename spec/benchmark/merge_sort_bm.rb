5718f0f43427fd6908b3265d4a54c55978d6ff95
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-04 16:52:12 -0700

    Benchmark merge_sort
    Update insertion_sort benchmark to match

--- spec/benchmark/merge_sort_bm.rb
+++ spec/benchmark/merge_sort_bm.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
-require 'insertion_sort'
+require 'merge_sort'
 require 'benchmark'
 
 space = "\n\n"
 puts space
 
-puts "Insertion Sort:"
+puts "Merge Sort:"
 puts "5_000 elements#{space}"
 
 l = 5_000
@@ -13,9 +13,9 @@ increasing = (1..l).to_a
 decreasing = (1..l).to_a.reverse
 random = (1..l).to_a.shuffle
 Benchmark.bm(15) do |x|
-  x.report("increasing:") { increasing.insertion_sort }
-  x.report("decreasing:") { decreasing.insertion_sort }
-  x.report("random:") { random.insertion_sort }
+  x.report("increasing:") { increasing.merge_sort }
+  x.report("decreasing:") { decreasing.merge_sort }
+  x.report("random:") { random.merge_sort }
 end
 
 puts space
